5b163a40e3844a659b4f8709fe0ceceeaa7a4e90
kevinvega14 <136644110+kevinvega14@users.noreply.github.com>
2026-02-08 14:28:50 -0300

    a ver :(
    x

---
+++ TP_ALGORIMOS_2026CV{GRUPO_3}/Primitivas/Arbol.h
@@ -0,0 +1,35 @@
+#ifndef ARBOL_H_INCLUDED
+#define ARBOL_H_INCLUDED
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TODO_OK 1
+#define SIN_MEM -2
+#define DUPLICADO -3
+#define ARBOL_VACIO -4
+
+
+typedef struct sNodoArbolBin
+{
+    void* info;
+    unsigned tamInfo;
+    struct sNodoArbolBin* hijoIzq;
+    struct sNodoArbolBin* hijoDer;
+}tNodoArbolBin;
+
+typedef tNodoArbolBin* tArbolBin;
+
+
+typedef int (*tCmp)(const void*, const void*);
+typedef void (*tAccion)(const void*);
+
+int InsertarEnArbolRecursivo(tArbolBin *p, const void* Dato, unsigned Bytes, tCmp cmp);
+void VaciarArbolBin(tArbolBin* p);
+void CrearArbolBin(tArbolBin* p);
+tArbolBin* BusquedaEnArbolRecursivo(tArbolBin *p, const void* Dato, tCmp cmp);
+
+
+#endif // ARBOL_H_INCLUDED
