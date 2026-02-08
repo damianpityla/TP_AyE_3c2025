98ce97f3527ca18f10a7a623198878de7aaf28e2
JuanMedina613 <juanignacio2005medina@gmail.com>
2026-02-08 14:12:12 -0300

    ahora si?
    .

--- TP_ALGORIMOS_2026CV{GRUPO_3}/Juego_Volcan/Usuario.h
+++ TP_ALGORIMOS_2026CV{GRUPO_3}/Juego_Volcan/Usuario.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "../Configuracion/Configuracion.h"
+#include "../Config/Configuracion.h"
 #include "../Primitivas/Arbol.h"
 #include "../Primitivas/Lista.h"
 
